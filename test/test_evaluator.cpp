ad87b579eb40bb722e87555a0cb5cf6f55c35321
squarematr1x <sasu.pirttipera@protonmail.com>
2022-10-13 16:17:59 +0300

    Add more tests

--- test/test_evaluator.cpp
+++ test/test_evaluator.cpp
@@ -13,7 +13,18 @@ TEST(EvaluatorTest, TestEvalIntegerExpr) {
         {"5", 5},
         {"17", 17},
         {"-99", -99},
-        {"-4", -4}
+        {"-4", -4},
+        {"5 + 5 + 3 + 5 - 10", 8},
+        {"2 * 2 * 2 * 2 * 2", 32},
+        {"-50 + 100 + -50", 0},
+        {"3 * 3 + 10", 19},
+        {"1 + 2 * 10", 21},
+        {"20 + 2 * -10", 0},
+        {"50 / 2 * 2 + 10", 60},
+        {"2 * (5 + 10)", 30},
+        {"3 * 3 * 3 + 10", 37},
+        {"3 * (3 * 3) + 10", 37},
+        {"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
     };
 
     for (const auto&  test : tests) {
@@ -34,7 +45,24 @@ TEST(EvaluatorTest, TestEvalBoolExpr) {
 
     std::vector<EvalTest> tests = {
         {"true", true},
-        {"false", false}
+        {"false", false},
+        {"0 < 2", true},
+        {"2 > 4", false},
+        {"1 < 1", false},
+        {"3 > 3", false},
+        {"1 == 1", true},
+        {"5 != 5", false},
+        {"1 == 2", false},
+        {"1 != 5", true},
+        {"true == true", true},
+        {"false == false", true},
+        {"true == false", false},
+        {"true != false", true},
+        {"false != true", true},
+        {"(11 < 55) == true", true},
+        {"(5 < 2) == true", false},
+        {"(11 > 55) == false", true},
+        {"(5 > 2) == true", true},
     };
 
     for (const auto&  test : tests) {
@@ -58,6 +86,7 @@ TEST(EvaluatorTest, TestEvanBangOperator) {
         {"!2", false},
         {"!!true", true},
         {"!!false", false},
+        {"!!!true", false},
         {"!!7", true}
     };
 
